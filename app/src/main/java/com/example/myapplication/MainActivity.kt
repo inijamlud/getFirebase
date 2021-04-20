98d1742fa5a7f20e37568e25574195089a708ccc
inijamlud <jamalude449@gmail.com>
2021-04-20 12:50:25 +0700

    add auth

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.firebaseapplication.FirebaseAdapter
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,22 +16,40 @@ class MainActivity : AppCompatActivity() {
     private lateinit var layoutku: RecyclerView
     private var ambilDatabase: ArrayList<FirebaseDataClassView> = arrayListOf()
     lateinit var btnAdd: View
+    lateinit var btnSignOut: View
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         layoutku = findViewById(R.id.rv_data)
         btnAdd = findViewById(R.id.btn_main_add)
+        btnSignOut = findViewById(R.id.btn_main_signout)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         fire = FirebaseDatabase.getInstance().getReference("employee")
         btnAdd.setOnClickListener{
             startActivity(Intent(this, AddActivity::class.java))
+        }
+
+        btnSignOut.setOnClickListener{
+            firebaseAuth.signOut()
+            checkme()
         }
     }
 
     override fun onStart(){
         super.onStart()
         loadMeFirst()
+        checkme()
+    }
+
+    fun checkme(){
+        val currentUser = firebaseAuth!!.currentUser
+        if (currentUser == null){
+            val pindah = Intent(this, LoginActivity::class.java)
+            startActivity(pindah)
+        }
     }
 
     private fun loadMeNow(){
